43c0f9b2c59ac09b0ab4659775ed3022cb97b855
Sandun de Silva <sandunsanjaya81@gmail.com>
2025-12-09 07:03:33 +0530

    add TodoList component and initialize todos in App component

---
+++ todo-list/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,29 @@
+export function TodoList({ todos }) {
+  return (
+    <section>
+      <h3>To-Do's</h3>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              name="completed"
+              defaultChecked={todo.completed}
+            />
+            <div>
+              {todo.name}
+              <br />
+              {todo.description}
+              <br />
+              {todo.deadline}
+              <br />
+              {todo.priority !== "none" && todo.priority}
+              <br />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
